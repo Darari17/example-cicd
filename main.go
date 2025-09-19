d81f755c45f5cc1e2575fea7574b0831db562746
farid <faridramadhan59@gmail.com>
2025-09-19 16:41:27 +0700

    feat: add gitignore and add /users endpoint

--- main.go
+++ main.go
@@ -12,6 +12,25 @@ func main() {
 	r.GET("/", func(ctx *gin.Context) {
 		ctx.String(http.StatusOK, "Welcome to Backend")
 	})
+	r.GET("/users", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"data": []struct {
+				Id   int
+				Name string
+			}{
+				{
+					Id:   1,
+					Name: "Farid",
+				}, {
+					Id:   2,
+					Name: "Rhamadhan",
+				}, {
+					Id:   3,
+					Name: "Darari",
+				},
+			},
+		})
+	})
 	r.Run(":8080")
 	log.Println("Backend is Running")
 }
